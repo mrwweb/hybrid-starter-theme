041f04040ee3892b8b22b0aa2e593cea83952f57
Mark Root-Wiley <info@mrwweb.com>
2021-11-24 10:35:50 -0800

    use a summary template part for all archives and search

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,11 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+		the_title( '<h1 class="entry-title">', '</h1>' );
 
 		if ( 'post' === get_post_type() ) :
 			?>
@@ -28,8 +24,6 @@
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
-
-	<?php _s_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
