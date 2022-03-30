17002d9b9f9536d04fc6f96e11d2096d7de87b85
Mark Root-Wiley <info@mrwweb.com>
2022-03-30 11:07:32 -0700

    add postcss logical properties to my build process
    missing

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,47 @@
-const { src, dest, watch, parallel } = require('gulp'),
-	sass = require('gulp-sass'),
-	postcss = require('gulp-postcss'),
-	sourcemaps = require('gulp-sourcemaps'),
-	autoprefixer = require('autoprefixer'),
-	babel = require('gulp-babel'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	livereload = require('gulp-livereload');
+const { src, dest, watch, parallel } = require( 'gulp' ),
+	sass = require( 'gulp-sass' ),
+	postcss = require( 'gulp-postcss' ),
+	sourcemaps = require( 'gulp-sourcemaps' ),
+	autoprefixer = require( 'autoprefixer' ),
+	postcssLogical = require( 'postcss-logical' ),
+	babel = require( 'gulp-babel' ),
+	concat = require( 'gulp-concat' ),
+	uglify = require( 'gulp-uglify' ),
+	livereload = require( 'gulp-livereload' );
 
 function css() {
-	return src('src/scss/**/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
-		.pipe(postcss([autoprefixer()]))
-		.pipe(sourcemaps.write('/maps'))
-		.pipe(dest('css'))
-		.pipe(livereload());
+	return src( 'src/scss/**/*.scss' )
+		.pipe( sourcemaps.init() )
+		.pipe( sass( { outputStyle: 'compressed' } ).on( 'error', sass.logError ) )
+		.pipe( postcss( [ postcssLogical(), autoprefixer() ] ) )
+		.pipe( sourcemaps.write( '/maps' ) )
+		.pipe( dest( 'css' ) )
+		.pipe( livereload() );
 }
 
 function js() {
-	return src(['src/js/*.js'])
-		.pipe(sourcemaps.init())
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
-		.pipe(concat('client-scripts.min.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('/maps'))
-		.pipe(dest('js'));
+	return src( [ 'src/js/*.js' ] )
+		.pipe( sourcemaps.init() )
+		.pipe( babel( {
+			presets: [ '@babel/env' ]
+		}) )
+		.pipe( concat( 'scripts.min.js' ) )
+		.pipe( uglify() )
+		.pipe( sourcemaps.write( '/maps' ) )
+		.pipe( dest( 'js' ) );
 }
 
 function editorJs() {
-	return src(['src/js/editor/*.js'])
-		.pipe(sourcemaps.init())
-		.pipe(babel({
-			presets: ['@babel/env','@babel/preset-react']
-		}))
-		.pipe(concat('client-editor.min.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('/maps'))
-		.pipe(dest('js'));
+	return src( [ 'src/js/editor/*.js' ] )
+		.pipe( sourcemaps.init() )
+		.pipe( babel( {
+			presets: [ '@babel/env', '@babel/preset-react' ]
+		} ) )
+		.pipe( concat( 'editor.min.js' ) )
+		.pipe( uglify() )
+		.pipe( sourcemaps.write( '/maps' ) )
+		.pipe( dest( 'js' ) );
 }
-
 
 function gulpWatch() {
 	livereload.listen();
