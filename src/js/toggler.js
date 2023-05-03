35e14fe139b4f651e5118720950d88dbe01964c5
Mark Root-Wiley <info@mrwweb.com>
2023-05-03 14:16:41 -0700

    toggler.js updates to not require wrapper container and close with ESC

--- src/js/toggler.js
+++ src/js/toggler.js
@@ -1,58 +1,51 @@
 /**
  * https://www.lottejackson.com/learning/a-reusable-javascript-toggle-pattern
  */
+'use strict';
 (function () {
-	'use strict';
+	const Toggler = function (toggleButton) {
+		let toggleTargetId, toggleTarget;
 
-	const Toggler = function(rootElement) {
-
-		var toggleButtons,
-			toggleButton,
-			toggleTarget,
-			target,
-			i;
-
-		this.init = function() {
-
-			toggleButton = rootElement.querySelector('.js-toggleButton');
-			
-			toggleTarget = toggleButton.getAttribute('aria-controls');
-			target = document.getElementById(toggleTarget);
-			target.classList.add('js-toggleTarget');
-			
+		this.init = function () {
+			toggleTargetId = toggleButton.getAttribute('aria-controls');
+			toggleTarget = document.getElementById(toggleTargetId);
+			toggleTarget.classList.add('wta-js-toggleTarget');
 			toggleButton.setAttribute('aria-expanded', 'false');
-			
 			toggleButton.addEventListener('click', toggle, false);
-
+			toggleButton.addEventListener('keyup', close, false);
+			toggleTarget.addEventListener('keyup', close, false);
 		};
 
-		var toggle = function(e) {
-
-			if ( 'false' === toggleButton.getAttribute('aria-expanded') ) {
-
+		const toggle = function (e) {
+			if ('false' === toggleButton.getAttribute('aria-expanded')) {
 				toggleButton.setAttribute('aria-expanded', 'true');
-
 			} else {
-
 				toggleButton.setAttribute('aria-expanded', 'false');
-
 			}
+			e.preventDefault();
+		};
 
+		const close = function (e) {
+			if (
+				27 === e.keyCode &&
+				'true' === toggleButton.getAttribute('aria-expanded')
+			) {
+				toggleButton.setAttribute('aria-expanded', 'false');
+				toggleButton.focus();
+			}
 			e.preventDefault();
 		}
-		
 	};
 
-	var toggles = document.querySelectorAll('.js-toggleWrapper'),
-		togglesTotal = toggles.length,
-		i,
-		toggle;
+	document.addEventListener('DOMContentLoaded', function() {
+		const toggles = document.querySelectorAll('.wta-js-toggleButton'),
+			togglesTotal = toggles.length;
+		let toggle;
 
-	for ( i = 0; i < togglesTotal; i = i + 1 ) {
-
-		//A new instance of the Toggler object is stored in the toggle variable on each iteration of the loop
-		toggle = new Toggler(toggles[i]);
-		toggle.init();
-
-	}
-}());
+		for (let i = 0; i < togglesTotal; i = i + 1) {
+			//A new instance of the Toggler object is stored in the toggle variable on each iteration of the loop
+			toggle = new Toggler(toggles[i]);
+			toggle.init();
+		}
+	});
+})();
