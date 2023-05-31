3c90c8a249fb6f40383f74f2a4e05741c613763e
Mark Root-Wiley <info@mrwweb.com>
2023-05-31 15:24:36 -0700

    add publishpress authors support by default

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -97,13 +97,24 @@ function _s_posted_on() {
  * Prints HTML with meta information for the current author.
  */
 function _s_posted_by() {
-	$byline = sprintf(
-		/* translators: %s: post author. */
-		esc_html_x( 'by %s', 'post author', '_s' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
+	if(
+		in_array(
+			'publishpress-authors/publishpress-authors.php',
+			apply_filters( 'active_plugins', get_option( 'active_plugins' ) )
+		)
+	) {
+		echo '<span class="byline"> by';
+		do_action( 'pp_multiple_authors_show_author_box', false, 'inline_avatar' );
+		echo '</span>';
+	} else {
+		$byline = sprintf(
+			/* translators: %s: post author. */
+			esc_html_x( 'by %s', 'post author', 'rvc' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
 
-	echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
 
 }
 
