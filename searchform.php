1ffd5ed78ac68f118e7b423237b3aa03f953e83e
Mark Root-Wiley <info@mrwweb.com>
2023-06-01 14:35:22 -0700

    require search input

--- searchform.php
+++ searchform.php
@@ -14,7 +14,7 @@ $search_form_counter++;
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="search-form__label screen-reader-text" for="s-<?php echo (int) $search_form_counter; ?>"><?php _ex( 'Search for:', 'label', '_s' ); ?>
 	</label>
-	<input type="search" class="search-form__input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" id="s-<?php echo (int) $search_form_counter; ?>">
+	<input type="search" class="search-form__input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" id="s-<?php echo (int) $search_form_counter; ?>" required>
 	<button type="submit" class="search-form__submit button button--small">
 		🔍
 		<span class="screen-reader-text"><?php echo esc_attr_x( 'Search', 'submit button', '_s' ); ?></span>
