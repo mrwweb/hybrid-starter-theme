db91e616307360488c043da85407c36cd8b47ff1
Mark Root-Wiley <info@mrwweb.com>
2023-10-04 12:02:42 -0700

    add more TEC files and make all but one a placeholder so I have to grab the most up-to-date version

---
+++ tribe/events/single-event/title.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * CHANGELOG
+ * 
+ * - Remove class from title so styles match rest of theme
+ */
+/**
+ * Single Event Title Template Part
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/single-event/title.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 4.7.2
+ *
+ */
+?>
+
+<?php the_title( '<h1>', '</h1>' );
