d3c3094988c238d88542fda37af9ba39b40c8afb
Mark Root-Wiley <info@mrwweb.com>
2023-10-05 12:05:47 -0700

    drop-down indicator icon

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -23,6 +23,22 @@ function body_classes( $classes ) {
 }
 add_filter( 'body_class', __NAMESPACE__ . '\body_classes' );
 
+add_filter( 'nav_menu_item_title', __NAMESPACE__ . '\add_dropdown_icon', 10, 4 );
+/**
+ * Add a dropdown indicator icon to menu items with children
+ */
+function add_dropdown_icon( $item_output, $item, $args, $depth ) {
+	
+	$has_children = in_array( 'menu-item-has-children', $item->classes, true );
+
+	if( $depth === 0 && $has_children ) {
+		$item_output = $item_output . get_svg( 'down-arrow', [ 'width' => '16', 'height' => '11' ], 'images' );
+	}
+		
+	return $item_output;
+
+}
+
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
