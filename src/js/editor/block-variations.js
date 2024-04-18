5944320993651b7a97fb71f36927b3aed187358e
Mark Root-Wiley <info@mrwweb.com>
2024-04-18 09:26:38 -0700

    update build process to just use npm scripts without gulp, reorganize my assets to match. update clicky-menus too

--- src/js/editor/block-variations.js
+++ src/js/editor/block-variations.js
@@ -1,33 +1,24 @@
 /* ESLINT config: */
-/* global wp */
 
 /* Adjust default Media & Text block*/
-wp.blocks.registerBlockVariation(
-	'core/media-text',
-	{
-		isDefault: true,
-		attributes: {
-			mediaPosition: 'right',
-			imageFill: true
-		},
-		innerBlocks: [
-			[ 'core/paragraph' ],
-		],
-	}
-);
+wp.blocks.registerBlockVariation('core/media-text', {
+	isDefault: true,
+	attributes: {
+		mediaPosition: 'right',
+		imageFill: true,
+	},
+	innerBlocks: [['core/paragraph']],
+});
 
 /* Latest Posts */
-wp.blocks.registerBlockVariation(
-	'core/latest-posts',
-	{
-		isDefault: true,
-		attributes: {
-			displayPostContent: true,
-			excerptLength: 50,
-			displayPostDate: true,
-			displayFeaturedImage: true,
-			featuredImageSizeSlug: 'medium',
-			addLinkToFeaturedImage: true,
-		},
-	}
-);
+wp.blocks.registerBlockVariation('core/latest-posts', {
+	isDefault: true,
+	attributes: {
+		displayPostContent: true,
+		excerptLength: 50,
+		displayPostDate: true,
+		displayFeaturedImage: true,
+		featuredImageSizeSlug: 'medium',
+		addLinkToFeaturedImage: true,
+	},
+});
