e4b5645d45835483a01fe1229cd393d4171b412b
Mark Root-Wiley <info@mrwweb.com>
2024-06-04 16:35:09 -0700

    fix template-part names in components/post-meta

--- components/post-meta.php
+++ components/post-meta.php
@@ -9,8 +9,8 @@ if ( 'post' === get_post_type() ) :
 	?>
 	<div class="entry-meta">
 		<?php
-		get_template_part( 'components/post-date.php' );
-		get_template_part( 'components/post-author.php' );
+		get_template_part( 'components/post-date' );
+		get_template_part( 'components/post-author' );
 		?>
 	</div><!-- .entry-meta -->
 <?php endif; ?>
