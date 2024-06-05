168e76f7edcf8a636e5727f5e920b550ed230732
Mark Root-Wiley <info@mrwweb.com>
2024-06-05 16:04:31 -0700

    put second container div in post-footer-*.php files to contrains width of child inline elements

--- components/post-footer-post.php
+++ components/post-footer-post.php
@@ -7,28 +7,46 @@
 
 ?>
 <footer class="<?php echo esc_attr( $args['class'] ); ?>">
-	<?php
-	/* translators: used between list items, there is a space after the comma */
-	$_mrw_categories_list = get_the_category_list( esc_html__( ', ', '_mrw' ) );
-	if ( $_mrw_categories_list ) {
-		/* translators: 1: list of categories. */
-		printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_mrw' ) . '</span>', $_mrw_categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	}
+	<div>
+		<?php
+		/* translators: used between list items, there is a space after the comma */
+		$trec_categories_list = get_the_category_list( esc_html__( ', ', 'trec' ) );
+		if ( $trec_categories_list ) {
+			/* translators: 1: list of categories. */
+			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'trec' ) . '</span>', $trec_categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		}
 
-	/* translators: used between list items, there is a space after the comma */
-	$_mrw_tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', '_mrw' ) );
-	if ( $_mrw_tags_list ) {
-		/* translators: 1: list of tags. */
-		printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', '_mrw' ) . '</span>', $_mrw_tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	}
+		/* translators: used between list items, there is a space after the comma */
+		$trec_tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'trec' ) );
+		if ( $trec_tags_list ) {
+			/* translators: 1: list of tags. */
+			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'trec' ) . '</span>', $trec_tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		}
 
-	if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link(
+		if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo '<span class="comments-link">';
+			comments_popup_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: post title */
+						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'trec' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
+			echo '</span>';
+		}
+
+		edit_post_link(
 			sprintf(
 				wp_kses(
-					/* translators: %s: post title */
-					__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', '_mrw' ),
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'trec' ),
 					array(
 						'span' => array(
 							'class' => array(),
@@ -36,26 +54,10 @@
 					)
 				),
 				wp_kses_post( get_the_title() )
-			)
-		);
-		echo '</span>';
-	}
-
-	edit_post_link(
-		sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Edit <span class="screen-reader-text">%s</span>', '_mrw' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
 			),
-			wp_kses_post( get_the_title() )
-		),
-		'<span class="edit-link">',
-		'</span>'
-	);
-	?>
+			'<span class="edit-link">',
+			'</span>'
+		);
+		?>
+	</div>
 </footer>
--- components/post-footer.php
+++ components/post-footer.php
@@ -8,24 +8,26 @@
 if ( is_user_logged_in() ) :
 	?>
 	<footer class="<?php echo esc_attr( $args['class'] ); ?>">
-		<?php
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', '_mrw' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
+		<div>
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'trec' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
 				),
-				wp_kses_post( get_the_title() )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
-		?>
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</div>
 	<footer>
 	<?php
 endif;
