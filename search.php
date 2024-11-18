8255bbaafbbde0b65b7ea60a07cf8af931538465
Mark Root-Wiley <info@mrwweb.com>
2024-11-18 12:59:24 -0800

    missing layout class on search page

--- search.php
+++ search.php
@@ -14,7 +14,7 @@ get_header();
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<header class="page-header is-layout-constrained">
 				<h1 class="page-title">
 					<?php
 					/* translators: %s: search query. */
