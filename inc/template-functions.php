5f91ea1f6a2d8fafa2796ddd3dfd01aaf20a3aec
Mark Root-Wiley <info@mrwweb.com>
2024-11-18 12:00:02 -0800

    remove pingback header, why was that there?

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -44,13 +44,3 @@ function add_dropdown_icon( $item_output, $item, $args, $depth ) {
 
 	return $item_output;
 }
-
-/**
- * Add a pingback url auto-discovery header for single posts, pages, or attachments.
- */
-function pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
-	}
-}
-add_action( 'wp_head', __NAMESPACE__ . '\pingback_header' );
