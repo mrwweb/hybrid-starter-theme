f95c29a715834b9381d8113a073890185e2b80aa
Mark Root-Wiley <info@mrwweb.com>
2025-02-24 12:08:03 -0800

    fix mistaken block template part on 404 page

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@ get_header();
 		</header><!-- .page-header -->
 
 		<div class="page-content is-root-container is-layout-constrained">
-			<?php block_template_part( 'footer' ); ?>
+			<?php block_template_part( '404' ); ?>
 		</div><!-- .page-content -->
 	</article><!-- .error-404 -->
 </main><!-- #main -->
