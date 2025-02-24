a614bbce95255f8894c74d99fd8900e105e23e39
Mark Root-Wiley <info@mrwweb.com>
2025-02-24 11:19:10 -0800

    make search.php use standard posts-loop template part

--- search.php
+++ search.php
@@ -24,20 +24,7 @@ get_header();
 			</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/summary', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
+			get_template_part( 'template-parts/posts-loop', 'search' );
 
 		else :
 
